66e91dd34fd83d39854ef05259c4d28acf077b61
sf7zm <202327064@std.sci.cu.edu.eg>
2026-04-30 18:28:19 +0300

    Update ArrayQueue.java

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,15 +1,20 @@
-public class ArrayQueue {
-   // private final int MAX_LENGTH;
-    private int [] arr ;
-    private int front ;
-    private int rear ;
-    private int count ;
+public class QueueArray<T>{
+    private final static int DEFAULT_CAPACITY = 10;
+    private T [] arr;
+    private int front;
+    private int rear;
+    private int count;
 
-    public ArrayQueue(int maxLength){
+    public QueueArray(int len){
+        if (len <= 0)
+            throw new IllegalArgumentException(len + "that is not a length for a queue");
+        this.arr = (T[]) new Object[len];
         this.front = 0;
-        this.rear = maxLength-1; ;
-        count = 0;
-        arr = new int[maxLength];
+        this.rear = -1;
+        this.count = 0;
+    }
+    public QueueArray(){
+        this(DEFAULT_CAPACITY);
     }
     public boolean isEmpty(){
         return count == 0;
@@ -17,42 +22,24 @@ public class ArrayQueue {
     public boolean isFull(){
         return count == arr.length;
     }
-    public void enQueue(int e) {
-        if (!isFull()) {
-            rear = (rear + 1) % arr.length;
-            arr[rear] = e;
-            count++;
-        }else
-            System.out.println("the queue is full");
+    public void enqueue(T e){
+        if (isFull())
+            throw new IllegalArgumentException("Queue is Full");
+        rear = (rear+1) % arr.length;
+        arr[rear] = e;
+        count++;
     }
-    public void deQueue(){
-        if (isEmpty()){
-            System.out.println("the queue is empty");
+    public void dequeue(){
+        if (isEmpty())
+            throw new IllegalArgumentException("Queue is Empty");
+        front = (front+1) % arr.length;
+        count--;
+    }
+    public void display(){
+        for (int i = 0, idx = front; i < count; i++) {
+            System.out.print(arr[idx] + " ");
+            idx = (idx + 1) % arr.length;
         }
-        else {
-           // arr[front] = 0;
-            front = (front+1)&arr.length;
-            count--;
-        }
-    }
-    public void getFront(){
-        if (isEmpty()){
-            System.out.println("Queue is empty");
-        }else
-            System.out.println(arr[front]);
-    }
-    public int getRear(){
-        return arr[rear];
-    }
-
-    public void printQueue(){
-       if (!isEmpty()){
-        for (int i = front; i != rear+1; i=(i+1)%arr.length) {
-            System.out.print(arr[i]+" ");
-        }
-           System.out.println();
-       }else
-           System.out.print("queue is empty");
-
+        System.out.println();
     }
 }
